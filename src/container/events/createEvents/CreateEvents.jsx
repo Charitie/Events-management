f5cd7643570605cf8731e5027220d1122a03fe32
charitie <charitytarus21@gmail.com>
2020-10-01 21:29:30 +0300

    implementing create events & fetch my events

--- src/container/events/createEvents/CreateEvents.jsx
+++ src/container/events/createEvents/CreateEvents.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import DatePicker from "react-datepicker";
+import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 
 import "react-datepicker/dist/react-datepicker.css";
 import "./CreateEvents.scss";
-import { useDispatch, useSelector } from "react-redux";
 import { createEvent } from "../../../redux/actions/eventsActions";
+import EventForm from "../../../components/eventForm/EventForm";
 
 const CreateEvents = (props) => {
 	const [formData, setFormData] = useState({
@@ -15,11 +15,11 @@ const CreateEvents = (props) => {
 		category: "",
 		location: "",
 		description: "",
-		// date: new Date(),
 	});
 	const [date, setDate] = useState(new Date());
-	const events = useSelector((state) => state.events);
-	const { loading, error, event } = events;
+	const [time, setTime] = useState(new Date());
+	// const events = useSelector((state) => state.events);
+	// const { loading, error, event } = events;
 	const dispatch = useDispatch();
 
 	const { name, category, location, description } = formData;
@@ -29,96 +29,46 @@ const CreateEvents = (props) => {
 		setFormData({ ...formData, [name]: value });
 	};
 
+	const handleDateChange = (date) => {
+		setDate(date)
+	}
+
+	const handleTimeChange = (date) => {
+		setTime(time)
+	}
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		dispatch(createEvent(name, category, location, description, date));
+		const eventDetails = {
+			name,
+			category,
+			location,
+			description,
+			date,
+			time: new Date(time).toTimeString().split(" ")[0],
+		};
+		dispatch(createEvent(eventDetails));
 	};
 
-	console.log("event", event);
-	// console.log("data", formData);
-	// console.log("date", date);
+	// if (!error) {
+	// 	props.history.push("/events/my-events");
+	// }
+
 
 	return (
 		<div className='container'>
 			<h1 className='title'>Create An Event</h1>
-			<form onSubmit={handleSubmit} className='form'>
-				<div className='form-group'>
-					<label className='form-group__label' htmlFor='name'>
-						Name:
-					</label>
-					<input
-						className='form-group__input'
-						type='text'
-						name='name'
-						id='name'
-						value={name}
-						onChange={handleChange}
-						// placeholder='Event name'
-					/>
-				</div>
-
-				<div className='form-group'>
-					<label className='form-group__label' htmlFor='category'>
-						Category:
-					</label>
-					<input
-						className='form-group__input'
-						type='text'
-						name='category'
-						id='category'
-						value={category}
-						onChange={handleChange}
-
-						// placeholder='Event category'
-					/>
-				</div>
-				<div className='form-group'>
-					<label className='form-group__label' htmlFor='location'>
-						Location:
-					</label>
-					<input
-						className='form-group__input'
-						type='text'
-						name='location'
-						id='location'
-						value={location}
-						onChange={handleChange}
-
-						// placeholder='Event location'
-					/>
-				</div>
-				<div className='form-group'>
-					<label className='form-group__label' htmlFor='description'>
-						Description:
-					</label>
-					<textarea
-						className='form-group__textarea'
-						name='description'
-						id='description'
-						value={description}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='form-group'>
-					<label className='form-group__label' htmlFor='date'>
-						date:
-					</label>
-					<DatePicker
-						className='form-group__date-picker'
-						peekNextMonth
-						showMonthDropdown
-						showYearDropdown
-						dropdownMode='select'
-						showTimeSelect
-						dateFormat='MMMM d, yyyy h:mm aa'
-						selected={date}
-						onChange={(date) => setDate(date)}
-					/>
-				</div>
-				<button type='submit' className='btn btn-gold'>
-					Create Event
-				</button>
-			</form>
+			<EventForm
+				formData={formData}
+				date={date}
+				time={time}
+				handleChange={handleChange}
+				handleTimeChange={handleTimeChange}
+				handleDateChange={handleDateChange}
+				handleSubmit={handleSubmit}
+				buttonText='Create Event'
+				buttonClassName='btn--gold'
+			/>
 		</div>
 	);
 };
--- src/container/events/myEvents/MyEvents.jsx
+++ src/container/events/myEvents/MyEvents.jsx
@@ -1,16 +1,154 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './MyEvents.scss';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./MyEvents.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { editMyEvent, getMyEvents } from "../../../redux/actions/eventsActions";
+import Modal from "../../../components/backdrop/Backdrop";
+import EventForm from "../../../components/eventForm/EventForm";
+import Backdrop from "../../../components/backdrop/Backdrop";
+import { ReactComponent as CancelButton } from "../../../images/cancel-circle.svg";
 
-const MyEvents = props => {
-  return (
-    <div>
-    </div>
-  )
-}
+const MyEvents = () => {
+	const [formData, setFormData] = useState({
+		id: "",
+		name: "",
+		category: "",
+		location: "",
+		description: "",
+	});
+	const [date, setDate] = useState(new Date());
+	const [time, setTime] = useState(new Date());
+	const [openModal, setOpenModal] = useState(false);
+	const myEvent = useSelector((state) => state.events);
+	const { events, error, loading } = myEvent;
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getMyEvents());
+		// dispatch(editMyEvent(event))
+		setFormData({
+			...formData,
+			id: events.id,
+			name: events.name,
+			category: events.category,
+			description: events.description,
+			location: events.location,
+		});
+		// setTime({ time: event.time });
+		// setDate({ date: event.date });
+	}, []);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleDateChange = (date) => {
+		setDate(date);
+	};
+
+	const handleTimeChange = (time) => {
+		setTime(time);
+	};
+
+	const handleSubmit = () => {
+		const eventDetails = {
+			...formData,
+			date,
+			time: new Date(time).toTimeString().split(" ")[0],
+		};
+		dispatch(editMyEvent(eventDetails));
+	};
+
+	const toggleModal = () => {
+		setOpenModal(!openModal);
+	};
+
+	return (
+		<div className='main-content'>
+			{openModal ? (
+				<>
+					<Backdrop />
+					<div className='form-modal'>
+						<div className='form-modal__header'>
+							<h2 className='form-modal__title'>Edit Event</h2>
+							<CancelButton
+								onClick={() => setOpenModal(false)}
+								className='form-modal__cancel-btn'
+							/>
+						</div>
+						<EventForm
+							date={date}
+							time={time}
+							formData={formData}
+							handleChange={handleChange}
+							handleTimeChange={handleTimeChange}
+							handleDateChange={handleDateChange}
+							handleSubmit={handleSubmit}
+							buttonText='Edit'
+							buttonClassName='btn--white'
+						/>
+					</div>
+				</>
+			) : (
+				<h1>null</h1>
+			)}
+			<div className='events'>
+				{events.length > 0 &&
+					events.map((event) => {
+						return (
+							<div key={event.id} className='event'>
+								<div className='title'>{event.name}</div>
+								<div className='content'>
+									<ul>
+										<li>
+											<span className='content__keys'> Location :</span>
+											{event.location}
+										</li>
+										<li>
+											<span className='content__keys'>Category :</span>
+											{event.category}
+										</li>
+										<li>
+											<span className='content__keys'> Date: </span>
+											{new Date(event.date).toDateString()}
+										</li>
+										<li>
+											<span className='content__keys'> Time: </span>
+											{event.time}
+										</li>
+										<li>
+											<span className='content__keys'>Description :</span>
+											{event.description}
+										</li>
+									</ul>
+									<div className='content__control-buttons'>
+										<span className='content__control-button content__control-button--guest'>
+											Guest
+										</span>
+										<span
+											onClick={toggleModal}
+											className='content__control-button content__control-button--edit'>
+											Edit
+										</span>
+										<span className='content__control-button content__control-button--delete'>
+											Delete
+										</span>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+			</div>
+		</div>
+	);
+};
 
 MyEvents.propTypes = {
+	events: PropTypes.object,
+	getMyEvents: PropTypes.func,
+	editMyEvent: PropTypes.func,
+};
 
-}
-
-export default MyEvents
+export default MyEvents;
