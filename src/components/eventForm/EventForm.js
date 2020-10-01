f0acda6c309e56c87a9e253d112e744bcbbb2878
charitie <charitytarus21@gmail.com>
2020-10-01 21:27:56 +0300

    implementing active links

---
+++ src/components/eventForm/EventForm.js
@@ -0,0 +1,130 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DatePicker, { CalendarContainer } from "react-datepicker";
+import "./EventForm.scss";
+
+const EventForm = ({
+	formData: { name, category, location, description },
+	date,
+	time,
+	handleSubmit,
+	handleChange,
+	handleTimeChange,
+	handleDateChange,
+	buttonText,
+	buttonClassName,
+}) => {
+	const MyContainer = ({ className, children }) => {
+		return (
+			<CalendarContainer className={className}>
+				<div style={{ position: "relative", fontSize: 10 }}>{children}</div>
+			</CalendarContainer>
+		);
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className='form'>
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='name'>
+					Name:
+				</label>
+				<input
+					className='form-group__input'
+					type='text'
+					name='name'
+					id='name'
+					value={name}
+					onChange={handleChange}
+					// placeholder='Event name'
+				/>
+			</div>
+
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='category'>
+					Category:
+				</label>
+				<input
+					className='form-group__input'
+					type='text'
+					name='category'
+					id='category'
+					value={category}
+					onChange={handleChange}
+
+					// placeholder='Event category'
+				/>
+			</div>
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='location'>
+					Location:
+				</label>
+				<input
+					className='form-group__input'
+					type='text'
+					name='location'
+					id='location'
+					value={location}
+					onChange={handleChange}
+
+					// placeholder='Event location'
+				/>
+			</div>
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='description'>
+					Description:
+				</label>
+				<textarea
+					className='form-group__textarea'
+					name='description'
+					id='description'
+					value={description}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='date'>
+					date:
+				</label>
+				<DatePicker
+					className='form-group__date-picker'
+					peekNextMonth
+					showMonthDropdown
+					showYearDropdown
+					dropdownMode='select'
+					dateFormat='yyyy/MM/dd'
+					isClearable
+					minDate={new Date()}
+					calendarContainer={MyContainer}
+					selected={date}
+					onChange={handleDateChange}
+				/>
+			</div>
+			<div className='form-group'>
+				<label className='form-group__label' htmlFor='date'>
+					time:
+				</label>
+				<DatePicker
+					className='form-group__date-picker'
+					showTimeSelect
+					showTimeSelectOnly
+					timeIntervals={15}
+					timeCaption='Time'
+					dateFormat='HH:mm aa'
+					isClearable
+					selected={time}
+					onChange={handleTimeChange}
+				/>
+			</div>
+
+			<button
+				type='submit'
+				className={`btn ${buttonClassName}`}>
+				{buttonText}
+			</button>
+		</form>
+	);
+};
+
+EventForm.propTypes = {};
+
+export default EventForm;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 
 import { IoMdLogOut } from "react-icons/io";
@@ -7,45 +7,71 @@ import { IoMdLogIn } from "react-icons/io";
 
 import logo from "../../images/logo3.Nt8vk";
 import "./Header.scss";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link, NavLink, useHistory, useRouteMatch } from "react-router-dom";
+import { logout } from "../../redux/actions/auth";
+import { useEffect } from "react";
 
-const Header = () => {
+const Header = ({ logout }) => {
 	const auth = useSelector((state) => state.auth);
 	const { isAuthenticated } = auth;
-	const  match  = useRouteMatch();
+	// const dispatch = useDispatch();
+	// const match = useRouteMatch();
+
+	const currentRoute = useHistory().location.pathname.toLowerCase();
 
 	const authHeader = (
 		<>
 			<nav className='header__nav header__nav--left'>
 				<ul className='menu-items'>
 					<li className='logo-item'>
+						{/* <NavLink 	 */}
 						<img src={logo} alt='site logo' className='header__logo' />
 					</li>
 					<li className='menu-item'>
-						<Link to='/events/create' className='header__link'>
-							Create Events
-						</Link>
-					</li>
-					<li className='menu-item'>
-						<Link to='/events/my-events' className='header__link'>
-							My Events
-						</Link>
-					</li>
-					<li className='menu-item'>
-						<Link to='/events' className='header__link'>
+						<NavLink
+							exact
+							to='/events'
+							activeClassName="header__link--active" 
+							className='header__link'
+							active
+						>
 							View All Events
-						</Link>
+						</NavLink>
+					</li>
+					<li className='menu-item'>
+						<NavLink
+							exact
+							to='/events/my-events'
+							className={
+								currentRoute.includes("/events/my-events")
+									? "header__link--active"
+									: "header__link"
+							}>
+							My Events
+						</NavLink>
+					</li>
+					<li className='menu-item'>
+						<NavLink
+							exact
+							to='/events/create'
+							className={
+								currentRoute.includes("create")
+									? "header__link--active"
+									: "header__link"
+							}>
+							Create Events
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
 			<nav className='header__nav header__nav--right'>
 				<ul>
 					<li className='user'>User</li>
-					<li className=''>
-						<Link to='/logout' className='header__link'>
-							Logout &nbsp;
-							<IoMdLogOut />
-						</Link>
+					<li onClick={logout} className=''>
+						{/* <NavLink  className='header__link'> */}
+						Logout &nbsp;
+						<IoMdLogOut />
+						{/* </Link> */}
 					</li>
 				</ul>
 			</nav>
@@ -59,10 +85,10 @@ const Header = () => {
 				<ul>
 					<li className=''>Events</li>
 					<li className=''>
-						<Link to='/login'>
+						<NavLink to='/login' className='header__link'>
 							<IoMdLogIn />
 							Login
-						</Link>
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
@@ -71,18 +97,17 @@ const Header = () => {
 
 	console.log("auth", isAuthenticated);
 	return (
-		<header className='header'>
-			{
-				// isAuthenticated ?
-				authHeader
-				// : guestHeader
-			}
+		<header className='app-container__header header'>
+			{/* {isAuthenticated ? */}
+			{authHeader}
+			{/* : guestHeader} */}
 		</header>
 	);
 };
 
 Header.prototype = {
 	isAuthenticated: PropTypes.bool,
+	logout: PropTypes.func,
 };
 
-export default Header;
+export default connect(null, { logout })(Header);
